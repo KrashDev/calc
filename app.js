e2513f57965763abef77aa1478d00429d59fb32d
donny <donny.gabai@gmail.com>
2019-01-25 23:17:06 -0500

    functionality

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 
 
+  //append numbers function
+  var i = 0;
+  for (i = 0; i < 10; i++) {
+    $('.numbers').append('<button class="' + i + '">' + i + '</button>');
+  }
+
 });
